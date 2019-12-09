f60aa0b9d0337f66d7c164cdb4ba1ecd9ce87d69
Adittia <adittia@doku.com>
2019-12-09 16:44:01 +0700

    initiate project

---
+++ src/main/java/com/spring/latihan/springbootadit/Springbootaditapp.java
@@ -0,0 +1,12 @@
+package com.spring.latihan.springbootadit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootaditapp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootaditapp.class, args);
+	}
+}
